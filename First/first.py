7823212907876108b3cf4e4226ad9cce7b69fcc2
mickami742 <mickami742@gmail.com>
2023-12-03 22:19:09 +0100

    first day second chapter partly

--- First/first.py
+++ First/first.py
@@ -1,7 +1,30 @@
+dictNums = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
 def readFile():
     file = open("input.txt", "r")
     tabOfCodes = file.read().split("\n")
     return tabOfCodes
+
+def findWordsInString(input):
+    tabofNums = []
+    for i in input:
+        tabOfIndexes = []
+        for word in dictNums.keys():
+            if word in i:
+                index = i.find(word)
+                tabOfIndexes.append(index)
+        tabofNums.append(sorted(tabOfIndexes))
+    return tabofNums
 
 def findFirstAndLastNumber(input):
     firstNumber = ""
@@ -26,7 +49,15 @@ def findFirstAndLastNumber(input):
 
 def main():
     tabOfCodes = readFile()
-    numbers = findFirstAndLastNumber(tabOfCodes)
-    print(sum(numbers))
+    # numbers = findFirstAndLastNumber(tabOfCodes)
+    # sumOfNumbers = sum(numbers)
+    # print(sumOfNumbers)
+    print(tabOfCodes)
+    newTab = findWordsInString(tabOfCodes)
+    # newNums = findFirstAndLastNumber(newTab)
+    print(newTab)
+    # print(newNums)
+    # print(sum(newNums))
+
 
 main()
